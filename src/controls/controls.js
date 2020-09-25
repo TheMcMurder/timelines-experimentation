4a3cb437bf3cd3eaf64101c7867199b1669e55c4
Justin McMurdie <mcmurdiej@gmail.com>
2020-09-25 11:48:13 -0600

    app controls

--- src/controls/controls.js
+++ src/controls/controls.js
@@ -5,16 +5,20 @@ export default function TimeControls() {
   return (
     <div className="bg-gray-400 absolute p-4 flex w-48 justify-between">
       <Button onClick={start}>Play</Button>
-      <Button onClick={pause}>Pause</Button>
+      <Button onClick={pause} secondary={true}>
+        Pause
+      </Button>
     </div>
   )
 }
 
 function Button(props) {
-  const { children, ...rest } = props
+  const { children, secondary = false, ...rest } = props
   return (
     <button
-      className="rounded-lg px-4 py-3 bg-teal-500 hover:bg-teal-600 text-white font-semibold leading-tight shadow-md"
+      className={`${
+        secondary ? 'bg-gray-700 hover:bg-gray-900' : 'bg-red-700 hover:bg-red-800'
+      } rounded-lg px-4 py-3 text-white font-semibold leading-tight shadow-md`}
       {...rest}
     >
       {children}
