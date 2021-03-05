a9c5cdfbbd5f0d67a79262038c8c20161f9f3a0a
Justin McMurdie <mcmurdiej@gmail.com>
2021-03-05 11:06:27 -0700

    removing API call - fixing data

--- src/covid-chart/data-stream/data-stream.js
+++ src/covid-chart/data-stream/data-stream.js
@@ -1,27 +1,47 @@
 import { useState, useEffect } from 'react'
-import { from, combineLatest } from 'rxjs'
+import { of, combineLatest } from 'rxjs'
 import { map, tap } from 'rxjs/operators'
 import { getAllEventsSmallerAndNextLarger } from './casualty-event-data.js'
 import { initializeTime } from '../controls/time-stream.js'
 import { timeState$, isRoughlySameDate } from '../controls/time-stream.js'
+import { exported } from './exported-data.js'
 
-const APIURL = 'https://api.covidtracking.com/v1/us/daily.json'
+// const APIURL = 'https://api.covidtracking.com/v1/us/daily.json'
 
 let lastDayOfData = new Date()
 
-const data$ = from(
-  fetch(APIURL).then((r) => {
-    return r.json()
-  }),
-).pipe(
+// I used to make this network request, but because I don't want to hit their server constantly for a project I'm not updating much I'm changing this to be data last pulled on March 5 2021 and chopping off every event in 2021
+// const data$ = from(
+//   fetch(APIURL).then((r) => {
+//     return r.json()
+//   }),
+// ).pipe(
+//   map((data) => {
+//     lastDayOfData = new Date(data[0].dateChecked)
+//     return data.reverse().map((d) => ({
+//       ...d,
+//       date: new Date(d.dateChecked),
+//     }))
+//   }),
+//   tap((data) => {
+//     console.log('data', JSON.stringify(data))
+//     initializeTime(data[0].date, data[data.length - 1].date)
+//   }),
+// )
+
+const data$ = of(exported).pipe(
   map((data) => {
-    lastDayOfData = new Date(data[0].dateChecked)
-    return data.reverse().map((d) => ({
+    lastDayOfData = new Date(data[data.length - 1].dateChecked)
+    return data.map((d) => ({
       ...d,
-      date: new Date(d.dateChecked),
+      date: new Date(d.date),
     }))
   }),
-  tap((data) => initializeTime(data[0].date, data[data.length - 1].date)),
+  tap((data) => {
+    const first = data[0].date
+    const last = data[data.length - 1].date
+    initializeTime(first, last)
+  }),
 )
 
 export const liveData$ = combineLatest([data$, timeState$]).pipe(
