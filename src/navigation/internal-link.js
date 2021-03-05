25b9feeaf290af19648d247435dce530d4bb54f0
Justin McMurdie <mcmurdiej@gmail.com>
2021-03-05 13:52:51 -0700

    updating link color

--- src/navigation/internal-link.js
+++ src/navigation/internal-link.js
@@ -4,7 +4,7 @@ import { Link } from '@reach/router'
 export default function ILink(props) {
   const { children, className, ...other } = props
   return (
-    <Link className={`font-medium text-gray-500 hover:text-gray-900 ${className ? className : ''}`} {...other}>
+    <Link className={`font-medium text-gray-500 hover:text-gray-200 ${className ? className : ''}`} {...other}>
       {children}
     </Link>
   )
