01b21f509be68d18a342dcd166be34562b50db85
Justin McMurdie <mcmurdiej@gmail.com>
2021-03-05 16:12:25 -0700

    Updating home text

--- src/home/home.js
+++ src/home/home.js
@@ -6,8 +6,8 @@ export default function HomePage() {
   return (
     <div className="px-4 sm:px-6 xl:px-8 pt-10 pb-16">
       <p className="text-2xl tracking-tight mb-10">
-        Thanks for visiting my experimental site. I like building visualizations for fun, here are some visualizations
-        I've made that I wanted to share.
+        Thanks for visiting my visualization playground. I like building visualizations for fun, here are some
+        visualizations I've made that I wanted to share.
       </p>
       <div className="flex flex-wrap">
         <FancyLink to="covid-19" title="Covid-19 and 2020 US Presidental race" />
