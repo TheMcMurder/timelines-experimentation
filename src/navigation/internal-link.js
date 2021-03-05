ffcef68e898a8d6c5cc9db399372fd93667cfa81
Justin McMurdie <mcmurdiej@gmail.com>
2021-03-05 14:11:37 -0700

    active links

--- src/navigation/internal-link.js
+++ src/navigation/internal-link.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Link } from '@reach/router'
 
 export default function ILink(props) {
-  const { children, className, ...other } = props
+  const { children, className = '', ...other } = props
+  const baseClasses = `font-medium text-gray-500 hover:text-gray-200 ${className}`
+  const activeClasses = `text-teal-400`
+  const getPropsHandler = useCallback(addClassIfActive(baseClasses, activeClasses), [])
   return (
-    <Link className={`font-medium text-gray-500 hover:text-gray-200 ${className ? className : ''}`} {...other}>
+    <Link getProps={getPropsHandler} className={baseClasses} {...other}>
       {children}
     </Link>
   )
+}
+
+function isActive(baseClasses, classesToAdd, { isPartiallyCurrent }) {
+  console.log('args', arguments)
+  return isPartiallyCurrent ? { className: `${baseClasses} ${classesToAdd}` } : { className: `${baseClasses}` }
+}
+
+function addClassIfActive(baseClasses, classesToAdd) {
+  return isActive.bind(null, baseClasses, classesToAdd)
 }
