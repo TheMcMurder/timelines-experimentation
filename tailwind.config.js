baf98ed8021a350fda2fc4bbc40f8c38343ed340
the_mcmurder <3059715+TheMcMurder@users.noreply.github.com>
2022-01-14 15:54:44 -0700

    upgrading to tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  darkMode: false, // or 'media' or 'class'
-  purge: ['./src/index.html', './src/**/*.js'],
+  content: ['./src/index.html', './src/**/*.js'],
   theme: {
     fontFamily: {
       header: ['Open Sans', 'sans-serif'],
